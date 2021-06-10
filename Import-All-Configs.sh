5dc99ac6a72baacaff9b39cb330c1230823af695
StudGaming <hussainkhorakiwala45@outlook.com>
2021-06-10 22:55:09 +0530

    Just something i'm working on

---
+++ Import-All-Configs.sh
@@ -0,0 +1,22 @@
+cd
+mkdir backups
+mv .config/alacritty backups
+mv .config/nvim backups
+mv .config/starship.toml backups
+mv .zshrc backups
+mv .vimrc backups
+mv .doom.d backups
+mv .vim backups
+git clone --branch=Main https://github.com/StudGaming/dotfiles
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+mkdir .config/alacritty
+mkdir .config/nvim
+mkdir .doom.d
+mv dotfiles/.config/alacritty/alacritty.yml .config/alacritty
+mv dotfiles/.config/nvim/init.vim .config/nvim
+mv dotfiles/.config/starship.toml .config/
+mv dotfiles/.zshrc ~
+mv dotfiles/.vimrc ~
