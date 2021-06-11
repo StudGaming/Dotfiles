43cae87062eb5f1c2ea20aa48dbab20e24e8990f
StudGaming <hussainkhorakiwala45@outlook.com>
2021-06-12 00:59:39 +0530

    Changes to the Import Script

--- Import-All-Configs.sh
+++ Import-All-Configs.sh
@@ -1,22 +1,47 @@
+########### Moving to the Home Directory ###########
 cd
+
+
+########### Taking Backups of Files if They Exist ###########
 mkdir backups
 mv .config/alacritty backups
+mv .config/qtile backups
 mv .config/nvim backups
+mv .config/rofi backups
 mv .config/starship.toml backups
-mv .zshrc backups
-mv .vimrc backups
 mv .doom.d backups
 mv .vim backups
-git clone --branch=Main https://github.com/StudGaming/dotfiles
+mv .vimrc backups
+mv .zshrc backups
+
+
+########### CLoning the Repository ###########
+git clone --branch=Beta https://github.com/StudGaming/dotfiles
+
+
+########### Getting Vim Plugged for Both NeoVim and Vim ###########
+#### Vim ####
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
 https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+#### NeoVim ####
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
 https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+
+########### Creating Directories ###########
+cd
 mkdir -p .config/alacritty
+mkdir -p .config/qtile
 mkdir -p .config/nvim
-mkdir .doom.d
+mkdir -p .config/rofi
+mkdir -p .doom.d
+
+
+########### Moving Files ###########
 mv dotfiles/.config/alacritty/alacritty.yml .config/alacritty
+mv dotfiles/.config/qtile/* .config/qtile
+mv dotfiles/.config/rofi/* .config/rofi
 mv dotfiles/.config/nvim/init.vim .config/nvim
 mv dotfiles/.config/starship.toml .config/
-mv dotfiles/.zshrc ~
-mv dotfiles/.vimrc ~
+mv dotfiles/.zshrc .
+mv dotfiles/.vimrc .
