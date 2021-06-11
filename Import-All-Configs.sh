2804ce1f63faa95240325ca717ef77e46014e87a
StudGaming <hussainkhorakiwala45@outlook.com>
2021-06-12 01:32:36 +0530

    Changes to the Import Script

--- Import-All-Configs.sh
+++ Import-All-Configs.sh
@@ -1,3 +1,7 @@
+########### Installing Packages ###########
+paru -S alacritty qtile vim neovim rofi zsh shell-color-scripts starship emacs
+
+
 ########### Taking Backups of Files if They Exist ###########
 cd
 mkdir backups
