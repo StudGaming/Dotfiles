09830c7b369d100484bf9784bb8b4f0f6c36dae5
Erik Dubois <erik.dubois@gmail.com>
2021-06-23 14:26:39 +0530

    Update Import-All-Configs.sh

--- Import-All-Configs.sh
+++ Import-All-Configs.sh
@@ -6,10 +6,12 @@ mv -rf .config/qtile backups
 mv -rf .config/nvim backups
 mv -rf .config/rofi backups
 mv -rf .config/starship.toml backups
-mv -rf .doom.d backups
-mv -rf .vim backups
+mv -rf .doom.d/ backups
+mv -rf .vim/ backups
 mv -rf .vimrc backups
 mv -rf .zshrc backups
+mv -rf .icons/ backups
+mv -rf .themes/ backups
 
 
 ########### CLoning the Directory ###########
@@ -28,23 +30,27 @@ https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 ########### Creating Directories ###########
 cd
-mkdir -p .config/alacritty
-mkdir -p .config/qtile
-mkdir -p .config/nvim
-mkdir -p .config/rofi
-mkdir -p .doom.d
+mkdir -p .config/alacritty/
+mkdir -p .config/qtile/
+mkdir -p .config/nvim/
+mkdir -p .config/rofi/
+mkdir -p .doom.d/
+mkdir -p .icons/
+mkdir -p .themes/
 
 
 ########### Moving Files ###########
 cd
-mv dotfiles/.config/alacritty/alacritty.yml .config/alacritty
-mv dotfiles/.config/qtile/* .config/qtile
-mv dotfiles/.config/rofi/* .config/rofi
-mv dotfiles/.config/nvim/init.vim .config/nvim
+mv dotfiles/.config/alacritty/alacritty.yml .config/alacritty/
+mv dotfiles/.config/qtile/* .config/qtile/
+mv dotfiles/.config/rofi/* .config/rofi/
+mv dotfiles/.config/nvim/init.vim .config/nvim/
 mv dotfiles/.config/starship.toml .config/
 mv dotfiles/.doom.d/* .doom.d/
-mv dotfiles/.zshrc .
+mv dotfiles/.icons/* .icons/
+mv dotfiles/.themes/* .themes/
 mv dotfiles/.vimrc .
+mv dotfiles/.zshrc .
 
 
 ########### Removing the Cloned Directory ###########
